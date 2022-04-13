426bbe606fbbd6653512699077fc53070a75f2b8
andre_profitfy <andreluiz@profitfy.me>
2022-04-13 14:30:27 -0300

    feat: consuming zip code api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import findCEP from 'cep-promise';
+
+interface IAddress {
+  city: string;
+  neighborhood: string;
+  state: string;
+  street: string;
+}
+
+const App: React.FC = () => {
+
+  const [zipCodeInputValue, setZipCodeInputValue] = React.useState<string>('');
+  const [address, setAddress] = React.useState<IAddress>({} as IAddress);
+
+  const handleZipCodeInputChange = React.useCallback((event: any) => {
+    setZipCodeInputValue(event.target.value);
+  }, [])
+
+  const getAddress = React.useCallback(async () => {
+    const data = await findCEP(zipCodeInputValue);
+
+    setAddress(data);
+  }, [zipCodeInputValue])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Consulta de CEP</h1>
+      <div style={{ display: 'flex', flexDirection: 'column' }} >
+        <label>Digite seu CEP</label>
+        <input
+          style={{ width: 400, marginBottom: 30 }}
+          type="text"
+          name="zip_code"
+          value={zipCodeInputValue}
+          onChange={handleZipCodeInputChange}
+        />
+
+        <label>Cidade</label>
+        <input type="text" defaultValue={address.city} disabled  style={{ width: 400 }} />
+
+        <label>Bairro</label>
+        <input type="text" defaultValue={address.neighborhood} disabled style={{ width: 400 }} />
+
+        <label>Estado</label>
+        <input type="text" defaultValue={address.state} disabled style={{ width: 400 }} />
+
+        <label>Rua</label>
+        <input type="text" defaultValue={address.street} disabled style={{ width: 400, marginBottom: 30 }} />
+      </div>
+
+      <button onClick={getAddress}>consultar</button>
     </div>
   );
 }
